eb14743600b2ecb30ea002bc1636837bc6fabb0a
sirar87 <sirars@gmail.com>
2020-08-23 17:00:01 +0200

    Add basic endpoint.

---
+++ src/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = 1337;
+
+app.get('/', function(request, response){
+    response.status(200).json({serverup:true});
+});
+  
+app.listen(port, function() {
+    console.log('App listening at http://localhost:' + port)
+});
